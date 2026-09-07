0f033c33c268f373fb2116e846b5a1bb1160c839
chisomdaniel <chinwezechisom@gmail.com>
2026-09-07 23:00:51 +0100

    added background video for hero section

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -11,7 +11,19 @@ export default function Hero() {
     >
       {/* overlay */}
       <div className="absolute inset-0 bg-background/30" />
-      <div className="relative min-h-162.5 xl:min-h-screen flex flex-col justify-center gap-6 xl:justify-between">
+
+      <video
+        autoPlay
+        muted={true}
+        loop
+        playsInline
+        poster="/globe-new-2.webp"
+        className="absolute top-0 left-0 w-full h-full object-cover z-0 pointer-events-none"
+      >
+        <source src="/video/globe-video.mp4" type="video/mp4" />
+        Browser does not support the video tag.
+      </video>
+      <div className="relative z-1 min-h-162.5 xl:min-h-screen flex flex-col justify-center gap-6 xl:justify-between">
         <H1 className="mb-10 text-center xl:text-start text-[50px] md:text-[70px] xl:text-[132px] font-medium text-foreground font-primary leading-12.5 md:leading-17.5 xl:leading-33 tracking-[-1px] md:tracking-[-1.4px] xl:tracking-[-2.64px]">
           Architecting the Modern Web
         </H1>
@@ -22,7 +34,7 @@ export default function Hero() {
           </ChildDiv>
 
           <ChildDiv className="max-w-82.5 md:max-w-125 flex flex-col items-center xl:items-start gap-10">
-            <CornerBox className="p-4 border border-green-a">
+            <CornerBox className="p-4 border border-green-a bg-green-light-a/30">
               <p className="text-center xl:text-start text-[18px] md:text-[20px] xl:text-[22px] leading-6 md:leading-6.75 xl:leading-7.5">
                 Hi, I&apos;m Daniel. Specializing in modern web technologies, I
                 turn complex ideas into fast, reliable, and maintainable
