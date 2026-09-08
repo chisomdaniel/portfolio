2b7d476157f5011267d6bd6ad11b0940f5a3d8a2
chisomdaniel <chinwezechisom@gmail.com>
2026-09-08 14:30:01 +0100

    background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${instrumentSans.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-background">
         <PageGrid />
         <Header />
         {children}
